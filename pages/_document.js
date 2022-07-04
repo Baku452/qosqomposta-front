19ad92cc51a32f2634cc2eebb65d59988dc34472
Aldair Huamani <aiacos12@outlook.com>
2022-07-04 13:10:17 -0500

    tag manager

--- pages/_document.js
+++ pages/_document.js
@@ -1,29 +1,38 @@
-import { Html, Head, Main, NextScript, Script } from 'next/document';
+/* eslint-disable @next/next/no-script-in-document */
+/* eslint-disable @next/next/no-document-import-in-page */
+import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
-    <Html>
-      <Head />
-      <body>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-53HXJRR"
-            height="0"
-            width="0"
-            style="display:none;visibility:hidden"
-          ></iframe>
-        </noscript>
-        <Main />
-        <NextScript />
+    <Html lang="es">
+      <Head>
         <Script
+          id="tag-manager"
           dangerouslySetInnerHTML={{
-            _html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-53HXJRR');`,
           }}
         ></Script>
+      </Head>
+      <body>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-53HXJRR"
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"
+          ></iframe>
+         `,
+          }}
+        ></noscript>
+        <Main />
+        <NextScript />
       </body>
     </Html>
   );
