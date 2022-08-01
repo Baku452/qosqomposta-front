c4014ab505676b726452aeb79530f1f2be82b935
Aldair Huamani <aiacos12@outlook.com>
2022-08-01 17:03:21 -0500

    remove animation on header and also translate

--- components/organism/landingBanner/bannerLanding.organism.jsx.jsx
+++ components/organism/landingBanner/bannerLanding.organism.jsx.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 const BannerLanding = () => {
   return (
     <div className="relative">
-      <div className="translate-y-1/2 animate-slideIn font-bold z-30 px-0 md:px-20 py-5 xl:py-24 mx-auto inset-x-0 absolute text-white">
+      <div className="translate-y-1/2 md:transform-none font-bold z-30 px-0 md:px-20 py-5 xl:py-24 mx-auto inset-x-0 absolute text-white">
         <div className="w-100 text-center">
           <h1 className="uppercase text-base xl:text-7xl py-5 lg:py-8">
             <span className="text-yellowQ">Qosqomposta</span> <br />
