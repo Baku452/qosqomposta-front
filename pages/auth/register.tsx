be144f391b4a9bb92606acb56a2a16dbde5724f6
Aldair Huamani <aiacos12@outlook.com>
2023-01-22 22:07:04 -0500

    fixes

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -3,23 +3,23 @@ import { AuthAction, withAuthUser } from 'next-firebase-auth';
 import { SignUpForm } from '@/components/molecules/signUpForm';
 
 const AuthPage = () => {
-  const [renderAuth, setRenderAuth] = useState(false);
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      setRenderAuth(true);
-    }
-  }, []);
+    const [renderAuth, setRenderAuth] = useState(false);
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            setRenderAuth(true);
+        }
+    }, []);
 
-  return (
-    <>
-      <div className="p-10  m-auto">
-        <h1>Bienvenido al Movimiento Compostero</h1>
-        {renderAuth ? <SignUpForm /> : null}
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="p-10  m-auto">
+                <h1>Bienvenido al Movimiento Compostero</h1>
+                {renderAuth ? <SignUpForm /> : null}
+            </div>
+        </>
+    );
 };
 
 export default withAuthUser({
-  whenAuthed: AuthAction.REDIRECT_TO_APP,
+    whenAuthed: AuthAction.REDIRECT_TO_APP,
 })(AuthPage);
