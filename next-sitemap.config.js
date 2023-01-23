80aa31a921fe2908cd01a1c20fbb6aa435748c62
Aldair Huamani <aiacos12@outlook.com>
2023-01-22 22:00:08 -0500

    Fix(-): fix renaming

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-const config = {
-  siteUrl: process.env.SITE_URL || 'https://qosqomposta.com/',
-  generateRobotsTxt: true, // (optional)
-};
-
-module.exports = config;
--- next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  images: {
-    domains: ['res.cloudinary.com'],
-    dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-  },
-  reactStrictMode: true,
-};
