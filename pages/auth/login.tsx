f3072382426cab1aad810b2d46e8f2ad182fd263
Aldair Huamani <aiacos12@outlook.com>
2023-01-22 22:06:59 -0500

    fixes

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -4,28 +4,28 @@ import { SignInForm } from '@/components/molecules/signInForm';
 import { SignUpForm } from '@/components/molecules/signUpForm';
 
 const SignUp = () => {
-  const [renderAuth, setRenderAuth] = useState(false);
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      setRenderAuth(true);
-    }
-  }, []);
+    const [renderAuth, setRenderAuth] = useState(false);
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            setRenderAuth(true);
+        }
+    }, []);
 
-  return (
-    <>
-      <div>
-        <h1>Login</h1>
+    return (
+        <>
+            <div>
+                <h1>Login</h1>
 
-        {renderAuth ? <SignUpForm /> : null}
-      </div>
-      <div className="bg-yellowQ w-full h-full">
-        Qosqomposta
-        <button className="btn btn-primary">Test</button>
-      </div>
-    </>
-  );
+                {renderAuth ? <SignUpForm /> : null}
+            </div>
+            <div className="bg-yellowQ w-full h-full">
+                Qosqomposta
+                <button className="btn btn-primary">Test</button>
+            </div>
+        </>
+    );
 };
 
 export default withAuthUser({
-  whenAuthed: AuthAction.REDIRECT_TO_APP,
+    whenAuthed: AuthAction.REDIRECT_TO_APP,
 })(SignUp);
