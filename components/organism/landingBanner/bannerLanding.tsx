1d0cd3f7c7997c2fc41833ed6a3b072eb77cca47
Aldair Huamani <aiacos12@outlook.com>
2023-02-01 23:15:52 -0500

    change banner url

--- components/organism/landingBanner/bannerLanding.tsx
+++ components/organism/landingBanner/bannerLanding.tsx
@@ -3,29 +3,29 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 const BannerLanding = () => {
-  return (
-    <div className="relative">
-      <div className="font-bold z-30 px-5 md:px-20 py-20 xl:py-24 mx-auto inset-x-0 absolute text-white">
-        <div className="w-100 text-center">
-          <h1 className="uppercase text-4xl lg:text-5xl xl:text-7xl py-5 lg:py-8">
-            <span className="text-yellowQ">Qosqomposta</span> <br />
-            <span className="text-2xl">Por un Cusco sostenible</span>
-          </h1>
-          <button className="btn btn-primary">
-            <Link href={'/'}>Únete Ya</Link>
-          </button>
+    return (
+        <div className="relative">
+            <div className="h-[calc(100vh_-_19rem)]">
+                <div className="font-bold z-30 px-5 md:px-20 py-20 xl:py-24 mx-auto inset-x-0 absolute text-white">
+                    <div className="w-100 text-center">
+                        <h1 className="uppercase text-4xl lg:text-5xl xl:text-7xl py-5 lg:py-8">
+                            <span className="text-yellowQ">Qosqomposta</span> <br />
+                            <span className="text-2xl">Por un Cusco sostenible</span>
+                        </h1>
+                        <button className="btn btn-primary">
+                            <Link href={'/'}>Únete Ya</Link>
+                        </button>
+                    </div>
+                </div>
+                <Image
+                    className="-z-50 backdrop-opacity-70 object-cover w-auto h-full"
+                    layout="fill"
+                    alt="bannerQosqomposta"
+                    src={WEB_BANNER_URL}
+                />
+            </div>
         </div>
-      </div>
-      <div className="h-[calc(100vh_-_19rem)]">
-        <Image
-          className="-z-50 backdrop-opacity-70 object-cover w-auto h-full"
-          layout="fill"
-          alt="bannerQosqomposta"
-          src={WEB_BANNER_URL}
-        />
-      </div>
-    </div>
-  );
+    );
 };
 
 export default BannerLanding;
--- main.config.ts
+++ main.config.ts
@@ -1,49 +1,49 @@
 export const WEB_BANNER_URL =
-  'https://res.cloudinary.com/https-qosqomposta-com/image/upload/v1673907770/web-home/Banner_tau3ji.jpg';
+    'https://res.cloudinary.com/https-qosqomposta-com/image/upload/v1675311230/web-home/Banner_kdm99t.jpg';
 
 export const PLAN_FAMILIAR_IMAGE =
-  'https://res.cloudinary.com/https-qosqomposta-com/image/upload/v1673919886/Planes_Familiares_lrgqbv.jpg';
+    'https://res.cloudinary.com/https-qosqomposta-com/image/upload/v1673919886/Planes_Familiares_lrgqbv.jpg';
 
 export const PLAN_COMERCIAL_IMAGE =
-  'https://res.cloudinary.com/https-qosqomposta-com/image/upload/v1673919885/Planes_Comerciales2_wzhw52.jpg';
+    'https://res.cloudinary.com/https-qosqomposta-com/image/upload/v1673919885/Planes_Comerciales2_wzhw52.jpg';
 
 export interface PLANS_HOME {
-  title: string;
-  itemsIncludes: PLANS_HOME_INCLUDES[];
+    title: string;
+    itemsIncludes: PLANS_HOME_INCLUDES[];
 }
 
 export interface PLANS_HOME_INCLUDES {
-  title: string;
-  key: string;
+    title: string;
+    key: string;
 }
 export const PLAN_FAMILIAR: PLANS_HOME = {
-  title: 'Planes Personales o Familiares',
-  itemsIncludes: [
-    {
-      title: 'Recolección de Residuos Orgánicos',
-      key: 'residuosOrgánicos',
-    },
-    {
-      title: 'Acopio de residuos orgánicos',
-      key: 'acopio',
-    },
-    {
-      title: 'Recojo de Reciclables o Eco Ladrillos',
-      key: 'ecoladrillos',
-    },
-  ],
+    title: 'Planes Personales o Familiares',
+    itemsIncludes: [
+        {
+            title: 'Recolección de Residuos Orgánicos',
+            key: 'residuosOrgánicos',
+        },
+        {
+            title: 'Acopio de residuos orgánicos',
+            key: 'acopio',
+        },
+        {
+            title: 'Recojo de Reciclables o Eco Ladrillos',
+            key: 'ecoladrillos',
+        },
+    ],
 };
 
 export const PLAN_COMERCIAL: PLANS_HOME = {
-  title: 'Planes Grupales o Comerciales',
-  itemsIncludes: [
-    {
-      title: 'Servicio de Barrio Compostero',
-      key: 'barrio',
-    },
-    {
-      title: 'Servicio de Compostaje Comercial',
-      key: 'comercial',
-    },
-  ],
+    title: 'Planes Grupales o Comerciales',
+    itemsIncludes: [
+        {
+            title: 'Servicio de Barrio Compostero',
+            key: 'barrio',
+        },
+        {
+            title: 'Servicio de Compostaje Comercial',
+            key: 'comercial',
+        },
+    ],
 };
