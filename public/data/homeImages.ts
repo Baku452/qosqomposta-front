c83ccbd934690ff6bd530b049cf6bb4779ab63f9
Aldair Huamani <aiacos12@outlook.com>
2023-04-17 20:38:42 -0500

    Fix(-): fix images url

--- public/data/homeImages.ts
+++ public/data/homeImages.ts
@@ -3,15 +3,15 @@ import { ImageWeb } from '@/types/mainTypes';
 export const IMAGES_SLIDER: ImageWeb[] = [
     {
         urlImage:
-            'https://res.cloudinary.com/https-qosqomposta-com/image/upload/v1678336164/fotos/slider_home2.jpg',
+            'https://res.cloudinary.com/https-qosqomposta-com/image/upload/v1678336164/movimiento%20qompostero/foto2%20esta%20realidad%20puede%20cambiar.jpg',
         altTitle: 'Slider Image',
     },
     {
         urlImage:
-            'https://res.cloudinary.com/https-qosqomposta-com/image/upload/v1678336164/fotos/slider_home.jpg',
+            'https://res.cloudinary.com/https-qosqomposta-com/image/upload/v1678336164/movimiento%20qompostero/foto%20esta%20realidad%20puede%20cambiar.jpg',
         altTitle: 'Slider Image 2',
     },
 ];
 
 export const LOGO_COLOR =
-    'https://res.cloudinary.com/https-qosqomposta-com/image/upload/v1677705346/logos%20solo%20condor/posta-logo-5-photoroom-png-photoroom_i3vmmp.svg';
+    'https://res.cloudinary.com/https-qosqomposta-com/image/upload/v1677705346/logos%20solo%20condor/logo%20condor%20color.svg';
