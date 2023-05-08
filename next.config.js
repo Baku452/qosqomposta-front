7e181be2976b0b670e31b31c20c1ff2800d1c220
Aldair Huamani <aiacos12@outlook.com>
2023-05-08 00:06:02 -0500

    FIx sass loader

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['res.cloudinary.com'],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+  sassOptions: {
+    fiber: false,
   },
 };
 module.exports = nextConfig;
