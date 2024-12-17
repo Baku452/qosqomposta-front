3a8a9e6b586ae290eab957122789e8b236b5e866
Aldair Huamani Cusi <32407895+Baku452@users.noreply.github.com>
2024-12-16 19:25:19 -0500

    Fix unused api

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -1,31 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react';
 import SignUpForm from '@/components/molecules/SignUpForm/SignUpForm';
-import { GetStaticProps, NextPage } from 'next';
+import { NextPage } from 'next';
 import 'react-phone-number-input/style.css';
-import { extractFields } from '@/utils/utils';
 
 export interface RegisterPageProps {
   data: any;
 }
-const RegisterPage: NextPage<RegisterPageProps> = ({ data }) => {
+const RegisterPage: NextPage<RegisterPageProps> = () => {
   return (
     <div className="p-10 m-auto text-center">
       <SignUpForm />
     </div>
   );
-};
-
-export const getStaticProps: GetStaticProps = async () => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_CITIES_API}`);
-  const data = await response.json();
-
-  const formattedData = extractFields(data.records);
-  return {
-    props: {
-      data: formattedData,
-    },
-  };
 };
 
 export default RegisterPage;
