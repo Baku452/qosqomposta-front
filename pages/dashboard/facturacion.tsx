fda4c687b1bb3a1a74d0277f944860a79b18e5c2
Aldair Huamani Cusi <32407895+Baku452@users.noreply.github.com>
2025-01-11 21:18:07 -0500

    Add(-): add placeholder

---
+++ pages/dashboard/facturacion.tsx
@@ -0,0 +1,8 @@
+import NoPageAvailable from '@/features/NoPageAvailable/NoPageAvailable';
+import { NextPage } from 'next';
+
+const FacturacionCustomerPage: NextPage = () => {
+  return <NoPageAvailable />;
+};
+
+export default FacturacionCustomerPage;
---
+++ pages/dashboard/faqs.tsx
@@ -0,0 +1,8 @@
+import NoPageAvailable from '@/features/NoPageAvailable/NoPageAvailable';
+import { NextPage } from 'next';
+
+const FAQSPage: NextPage = () => {
+  return <NoPageAvailable />;
+};
+
+export default FAQSPage;
