ac3b4845339e0bc2a3034405d9f8bc6707ac2dd6
Aldair Huamani Cusi <32407895+Baku452@users.noreply.github.com>
2025-01-17 12:30:44 -0500

    Add(-): add servicio activo

--- components/atoms/NoRecords/NoRecords.tsx
+++ components/atoms/NoRecords/NoRecords.tsx
@@ -1,15 +1,30 @@
+import React from 'react';
+import { IconType } from 'react-icons';
 import { BiSearch } from 'react-icons/bi';
 
 export interface NoRecordsProps {
   message?: string;
+  CustomIcon?: IconType;
+  iconSize?: number;
+  className?: string;
+  messageClassname?: string;
 }
-export const NoRecords: React.FC<NoRecordsProps> = ({ message }) => {
+
+export const NoRecords: React.FC<NoRecordsProps> = ({
+  message = 'No se han encontrado resultados',
+  CustomIcon,
+  iconSize = 30,
+  className,
+  messageClassname,
+}) => {
   return (
-    <div className="flex flex-col items-center p-5 text-center text-gray-500">
-      <BiSearch size={30} />
-      <h3 className="font-paragraph">
-        {message ? message : 'No se han encontrado resultados'}
-      </h3>
+    <div
+      className={`${className} flex flex-col items-center justify-center p-5 text-center text-gray-500`}
+    >
+      {CustomIcon ? <CustomIcon size={iconSize} /> : <BiSearch size={iconSize} />}
+      <h3 className={`font-paragraph ${messageClassname}`}>{message}</h3>
     </div>
   );
 };
+
+export default NoRecords;
